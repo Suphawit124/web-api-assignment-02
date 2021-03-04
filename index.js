31ec107f8a45d7087f27ba99c5cd6fd7906f3978
Suphawit Watthanataetakun <61160124@go.buu.ac.th>
2021-03-04 19:28:49 +0700

    update GET /books/:id to retrieve data from MongoDB

--- index.js
+++ index.js
@@ -44,5 +44,16 @@ app.post('/books', async (req, res) => {
     res.status(201).json(bookID)
 })
 
+app.get('/books/:id', async (req, res) => {
+    //input
+    let id = req.params.id
+
+    //process
+    const book = await booksCollection.findOne({ _id: ObjectID(id) })
+
+    //output
+    res.status(200).json(book)
+})
+
 const port = 3000
 app.listen(3000, () => console.log(`Server started at ${port}`))
